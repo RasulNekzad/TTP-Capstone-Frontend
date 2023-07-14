cb179809fb9219eb0db085f0d751b1591c8a024d
Jerry Jiang <jerryjiang49@gmail.com>
2023-07-14 13:52:44 -0400

    Add: navbar
    Other Changes
    1) Changed app name in index.html
    2) Imported bootstrap css into index.js
    3) (WIP) add home route to app.js

---
+++ src/components/layout/TopNavbar.js
@@ -0,0 +1,43 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './TopNavbar.css';
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+function TopNavbar() {
+    // checks if the screen size is less than 992px, in which case the navbar will collapse
+    const [navCollapse, setNavCollapse] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const screenSize = window.matchMedia('(max-width: 992px)').matches;
+            setNavCollapse(screenSize);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+                <Navbar.Brand href="#brand" className={`${navCollapse ? 'brand' : 'brand-padding'}`}>Spotify Proximity</Navbar.Brand>
+                <Navbar.Toggle id="basic-navbar-toggle" aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="/songs" style={navCollapse ? { borderTop: '1px solid black' } : {}} className={`${navCollapse ? 'row-padding' : 'link-padding'} link-dec`}>Songs</Link>
+                        <Link to="/history" className={`${navCollapse ? 'row-padding' : ''} link-dec`}>History</Link>
+                    </Nav>
+                    <Nav className={`${navCollapse ? '' : 'nav-padding'}`}>
+                        <Link to="/login" className={`${navCollapse ? 'row-padding' : 'link-padding'} link-dec`}>Login</Link>
+                        <Link to="/signup" className={`${navCollapse ? 'row-padding' : 'link-signup'} link-dec`}>Sign Up</Link>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default TopNavbar;
