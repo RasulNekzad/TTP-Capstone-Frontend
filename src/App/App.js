c83b20ce369b07bdadea1accc59c53f96ec52599
Rasul Nekzad <nekzadrasul@gmail.com>
2023-07-18 22:55:23 -0400

    Added mock interval to ping spotify API, needs Auth to continue development

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,40 @@
 // import { useDispatch, useSelector } from "react-redux";
 // import { useEffect } from "react";
 // import { Button } from "react-bootstrap";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages";
 import Auth from "../pages/auth";
 import TopNavbar from "../components/layout/TopNavbar";
-import User from "../pages/user"
+import User from "../pages/user";
 import PlaybacksNearby from "../pages/playbacksNearby";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 function App() {
+  const thirySecondsMs = 30000;
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      const localAuth = true; // TODO: Get logged in auth from context (firebase)
+      if (localAuth) {
+        const spotifyAuth = true; // TODO: Get spotify logged in auth from context (firebase)
+        if (spotifyAuth) {
+          // Ping spotfiy API
+          console.log(
+            "Simulating pinging spotify API for current track played by user..."
+          );
+        }
+      } else {
+        // OPTIONS:
+        // 1, Immediate redirect to login page
+        // 2. Prompt user to login with toastr
+      }
+    }, thirySecondsMs);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   /**
    * Commented out tests
    */
@@ -25,7 +50,7 @@ function App() {
   //   );
   // }
 
-  // useEffect(() => {    
+  // useEffect(() => {
   // }, [item]);
 
   return (
@@ -34,14 +59,14 @@ function App() {
     //   {item ? <h1>{item.name}</h1> : <h1>Loading</h1>} */}
     // </div>
     <Router>
-        <TopNavbar/>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/login" element={<Auth/>}/>
-                <Route path="/" element={<Home/>} />
-                <Route path="/user/:id" element={<User />} />
-                <Route path="/songs" element={<PlaybacksNearby />} />
-            </Routes>
+      <TopNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user/:id" element={<User />} />
+        <Route path="/songs" element={<PlaybacksNearby />} />
+      </Routes>
     </Router>
   );
 }
